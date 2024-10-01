ec0d884e335ed89291bc166fce54a33c8c893ddd
Judanana <moussaadamou898@gmail.com>
2024-10-01 23:17:07 +0200

    add ex05 and ex06 cpp01

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -6,12 +6,13 @@
 /*   By: madamou <madamou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 17:39:02 by madamou           #+#    #+#             */
-/*   Updated: 2024/10/01 22:03:09 by madamou          ###   ########.fr       */
+/*   Updated: 2024/10/01 23:13:44 by madamou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 #include <cstdio>
+#include <iterator>
 #include <string>
 
 Harl::Harl(void) {
@@ -23,35 +24,55 @@ Harl::~Harl(void) {
 }
 
 void Harl::debug(void) {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << std::endl;
 }
 
 void Harl::info(void) {
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
 }
 
 void Harl::warning(void) {
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void) {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
+}
+
+void nl_cout(void) {
+	std::cout << std::endl;
 }
 
 void Harl::complain(std::string level) {
 	std::string tab[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void (Harl::*ptr[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-	Harl func;
 	int i;
 
 	i = 0;
-	while (i < 4)
-	{
+	while (i < 4) {
 		if (tab[i] == level) {
-			(func.*ptr[i])();
-			return ;
+			break;
 		}
 		i += 1;
 	}
-	std::cout << "Not a good level (not found for function tab) !!" << std::endl;
+	switch (i) {
+		case 0:
+			(debug(), nl_cout(), info(), nl_cout(), warning(), nl_cout(), error());
+			break;
+		case 1:
+			(info(), nl_cout(), warning(), nl_cout(), error());
+			break;
+		case 2:
+			(warning(), nl_cout(), error());
+			break;
+		case 3:
+			error();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+			break;
+	}
 }
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,40 +6,20 @@
 /*   By: madamou <madamou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 22:40:00 by madamou           #+#    #+#             */
-/*   Updated: 2024/10/01 22:00:45 by madamou          ###   ########.fr       */
+/*   Updated: 2024/10/01 23:07:36 by madamou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main(void) {
-    Harl harl;
-
-    // Test pour le niveau DEBUG
-    std::cout << "=== Test DEBUG ===" << std::endl;
-    harl.complain("DEBUG");
-    std::cout << std::endl;
-
-    // Test pour le niveau INFO
-    std::cout << "=== Test INFO ===" << std::endl;
-    harl.complain("INFO");
-    std::cout << std::endl;
-
-    // Test pour le niveau WARNING
-    std::cout << "=== Test WARNING ===" << std::endl;
-    harl.complain("WARNING");
-    std::cout << std::endl;
-
-    // Test pour le niveau ERROR
-    std::cout << "=== Test ERROR ===" << std::endl;
-    harl.complain("ERROR");
-    std::cout << std::endl;
-
-    // Test pour un niveau inconnu
-    std::cout << "=== Test NIVEAU INCONNU ===" << std::endl;
-    harl.complain("UNKNOWN");
-    std::cout << std::endl;
-
+int main(int argc, char **argv) {
+    Harl comment;
+    
+    if (argc != 2) {
+        std::cout << "Not good arguments number !! \nYou can't complain too much" << std::endl;
+        return 1;
+    }
+    comment.complain(std::string(argv[1]));
     return 0;
 }
 
